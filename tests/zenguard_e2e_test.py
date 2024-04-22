0de33d18d3cd86ea067dc4bf9acf0705251e763b
Baur <baur.krykpayev@gmail.com>
2024-04-22 12:38:45 -0400

    [sdk] put back main

--- tests/zenguard_e2e_test.py
+++ tests/zenguard_e2e_test.py
@@ -2,11 +2,6 @@ import os
 
 from zenguard.zenguard import Credentials, Detector, ZenGuard, ZenGuardConfig
 
-api_key = os.environ.get("ZEN_API_KEY")
-if not api_key:
-    raise ValueError("ZEN_API_KEY is not set")
-config = ZenGuardConfig(credentials=Credentials(api_key=api_key))
-zenguard = ZenGuard(config=config)
 
 def assert_successful_response_not_detected(response):
     assert response is not None
@@ -53,3 +48,17 @@ def test_secrets():
     detectors = [Detector.SECRETS]
     response = zenguard.detect(detectors=detectors, prompt=prompt)
     assert_successful_response_not_detected(response)
+
+if __name__ == "__main__":
+    api_key = os.environ.get("ZEN_API_KEY")
+    if not api_key:
+        raise ValueError("ZEN_API_KEY is not set")
+    config = ZenGuardConfig(credentials=Credentials(api_key=api_key))
+    zenguard = ZenGuard(config=config)
+
+    test_prompt_injection(zenguard)
+    test_pii(zenguard)
+    test_allowed_topics(zenguard)
+    test_banned_topics(zenguard)
+    test_keywords(zenguard)
+    print("All tests passed!")
