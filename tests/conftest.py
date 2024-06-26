0312d908c89db213b1a5aa0be44da6fc9e3b2c23
Nuradil <nuradil.maksut@icloud.com>
2024-06-26 23:26:19 +0500

    created fixture clear_cache for clear cache before tests and created fixture zen_api_key for check api_key and get from env and configured fixture scopes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,30 +1,41 @@
 import os
 
 import pytest
+import httpx
 from openai import OpenAI
 
 from zenguard.zenguard import Credentials, SupportedLLMs, ZenGuard, ZenGuardConfig
 
 
-@pytest.fixture(scope="session", autouse=True)
-def zenguard():
+@pytest.fixture(scope="session")
+def zen_api_key():
     api_key = os.environ.get("ZEN_API_KEY")
     assert api_key, "ZEN_API_KEY is not set"
-    config = ZenGuardConfig(credentials=Credentials(api_key=api_key))
+    return api_key
+
+
+@pytest.fixture(scope="session", autouse=True)
+def clear_cache(zen_api_key):
+    response = httpx.put(
+        url="https://api.zenguard.ai/v1/cache/clear", headers={"x-api-key": zen_api_key}
+    )
+    response.raise_for_status()
+
+
+@pytest.fixture(scope="module")
+def zenguard(zen_api_key):
+    config = ZenGuardConfig(credentials=Credentials(api_key=zen_api_key))
     return ZenGuard(config=config)
 
 
-@pytest.fixture(scope="session", autouse=True)
-def zenguard_openai():
-    api_key = os.environ.get("ZEN_API_KEY")
-    assert api_key, "ZEN_API_KEY is not set"
-
+@pytest.fixture(scope="module")
+def zenguard_openai(zen_api_key):
     openai_key = os.environ.get("OPENAI_API_KEY")
     assert openai_key, "OPENAI_API_KEY is not set"
 
     openai_client = OpenAI(api_key=openai_key)
     config = ZenGuardConfig(
-        credentials=Credentials(api_key=api_key),
+        credentials=Credentials(api_key=zen_api_key),
         ai_client=openai_client,
         llm=SupportedLLMs.CHATGPT,
     )
